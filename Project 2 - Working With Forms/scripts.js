a6205f81be1cd919aedc98dd77a5c50b6a84c310
Angela Caldas <aes.caldas@gmail.com>
2023-05-08 17:49:26 -0300

    feat: use v-model to get user input data

--- Project 2 - Working With Forms/scripts.js
+++ Project 2 - Working With Forms/scripts.js
@@ -12,13 +12,18 @@ members = [
 ];
 
 const handlingForms = {
-
   data() {
     return {
-      members: window.members
+      members: members,
+      // Aqui guardaremos os valores inseridos pelo usuário nos inputs do formulário, a serem capturados via diretiva `v-model`. Podemos declarar as propriedades como `null` ou simplesmente deixar o objeto vazio, que o v-model automaticamente cria as propriedades com seus valores
+      newMember: {}
     };
   },
-
+  methods: {
+    addMember: function () {
+      this.members.push(this.newMember);
+    }
+  }
 };
 
 Vue.createApp(handlingForms).mount('#app');
